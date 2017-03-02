38b10b8eb7c0b4481ca0bf508b656d98509474ba
Boopesh <boopeshmahendran@gmail.com>
2017-03-02 16:57:34 +0530

    Added clickHandler to calculator buttons and created two new buttons 'clear' and 'square root'

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -9,34 +9,51 @@ class Calculator extends Component {
       value: 0
     };
   }
+  handleClickButton(buttonValue) {
+    if (typeof buttonValue === 'number') {
+      this.setState({
+        value: (this.state.value === 0)? buttonValue: this.state.value * 10 + buttonValue
+      });
+      return ;
+    }
+    
+    switch(buttonValue) {
+      case 'C': this.setState({value: 0});
+        break;
+    }
+  }
   render() {
     return (
       <div className='calculator'>
         <div className='calculator__display'> {this.state.value} </div>
         <Grid className='calculator__buttons'>
-          <Row>
-            <Col xs={3} className='calculator__button'><Button>7</Button></Col>
-            <Col xs={3} className='calculator__button'><Button>8</Button></Col>
-            <Col xs={3} className='calculator__button'><Button>9</Button></Col>
-            <Col xs={3} className='calculator__button'><Button>+</Button></Col>
+          <Row> 
+            <Col xs={6} className='calculator__button'><Button onClick={() => this.handleClickButton('C')}>CLR</Button></Col>
+            <Col xs={6} className='calculator__button'><Button onClick={() => this.handleClickButton('C')}>√</Button></Col>
+          </Row>
+          <Row> 
+            <Col xs={3} className='calculator__button'><Button onClick={() => this.handleClickButton(7)}>7</Button></Col>
+            <Col xs={3} className='calculator__button'><Button onClick={() => this.handleClickButton(8)}>8</Button></Col>
+            <Col xs={3} className='calculator__button'><Button onClick={() => this.handleClickButton(9)}>9</Button></Col>
+            <Col xs={3} className='calculator__button'><Button onClick={() => this.handleClickButton('+')}>+</Button></Col>
           </Row>
           <Row>
-            <Col xs={3} className='calculator__button'><Button>4</Button></Col>
-            <Col xs={3} className='calculator__button'><Button>5</Button></Col>
-            <Col xs={3} className='calculator__button'><Button>6</Button></Col>
-            <Col xs={3} className='calculator__button'><Button>-</Button></Col>
+            <Col xs={3} className='calculator__button'><Button onClick={() => this.handleClickButton(4)}>4</Button></Col>
+            <Col xs={3} className='calculator__button'><Button onClick={() => this.handleClickButton(5)}>5</Button></Col>
+            <Col xs={3} className='calculator__button'><Button onClick={() => this.handleClickButton(6)}>6</Button></Col>
+            <Col xs={3} className='calculator__button'><Button onClick={() => this.handleClickButton('-')}>-</Button></Col>
           </Row>
           <Row>
-            <Col xs={3} className='calculator__button'><Button>1</Button></Col>
-            <Col xs={3} className='calculator__button'><Button>2</Button></Col>
-            <Col xs={3} className='calculator__button'><Button>3</Button></Col>
-            <Col xs={3} className='calculator__button'><Button>*</Button></Col>
+            <Col xs={3} className='calculator__button'><Button onClick={() => this.handleClickButton(1)}>1</Button></Col>
+            <Col xs={3} className='calculator__button'><Button onClick={() => this.handleClickButton(2)}>2</Button></Col>
+            <Col xs={3} className='calculator__button'><Button onClick={() => this.handleClickButton(3)}>3</Button></Col>
+            <Col xs={3} className='calculator__button'><Button onClick={() => this.handleClickButton('*')}>*</Button></Col>
           </Row>
           <Row>
-            <Col xs={3} className='calculator__button'><Button>0</Button></Col>
-            <Col xs={3} className='calculator__button'><Button>.</Button></Col>
-            <Col xs={3} className='calculator__button'><Button>/</Button></Col>
-            <Col xs={3} className='calculator__button'><Button>=</Button></Col>
+            <Col xs={3} className='calculator__button'><Button onClick={() => this.handleClickButton(0)}>0</Button></Col>
+            <Col xs={3} className='calculator__button'><Button onClick={() => this.handleClickButton('.')}>.</Button></Col>
+            <Col xs={3} className='calculator__button'><Button onClick={() => this.handleClickButton('/')}>/</Button></Col>
+            <Col xs={3} className='calculator__button'><Button onClick={() => this.handleClickButton('=')}>=</Button></Col>
           </Row>
         </Grid>
       </div>
